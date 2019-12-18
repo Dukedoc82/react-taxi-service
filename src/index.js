2698b5a3c88625c9f32086a6293d00c1e39d84b6
Dmitry Dyukov <dmitry_dyukov@epam.com>
2019-12-18 23:08:05 +0300

    Implement designed login form

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './login/Login'
 import OrdersPage from './content/OrdersPage'
+//import ModelForm from './login/ModalForm'
+import ModalForm2 from './login/ModalForm2'
 
-if (localStorage.getItem("userToken")) {
+/*if (localStorage.getItem("userToken")) {
     ReactDOM.render(<OrdersPage />, document.getElementById('root'));
-} else {
-    ReactDOM.render(<Login />, document.getElementById('root'));
-}
+} else {*/
+    //ReactDOM.render(<ModelForm />, document.getElementById('root'));
+//}
+
+ReactDOM.render(<ModalForm2/>, document.getElementById('root'));
