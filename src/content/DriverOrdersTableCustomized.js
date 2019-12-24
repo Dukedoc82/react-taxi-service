2da0d0eca52b08674a249ef10487c3277b502fb1
Dmitry Dyukov <dmitry_dyukov@epam.com>
2019-12-24 12:29:47 +0300

    Implement data load into customized driver orders table.

--- src/content/DriverOrdersTableCustomized.js
+++ src/content/DriverOrdersTableCustomized.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { lighten, makeStyles } from '@material-ui/core/styles';
@@ -16,30 +16,14 @@ import Paper from '@material-ui/core/Paper';
 import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
 import DeleteIcon from '@material-ui/icons/Delete';
 import FilterListIcon from '@material-ui/icons/FilterList';
+import {makeGetCall} from "../utils/ajaxRequest";
+import {getStatusCaption, getUserFullName} from "../utils/DataUtils";
 
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
+function createData(addressFrom, addressTo, appointmentTime, client, status) {
+    return { addressFrom, addressTo, appointmentTime, client, status };
 }
-
-const rows = [
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Donut', 452, 25.0, 51, 4.9),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Honeycomb', 408, 3.2, 87, 6.5),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Jelly Bean', 375, 0.0, 94, 0.0),
-    createData('KitKat', 518, 26.0, 65, 7.0),
-    createData('Lollipop', 392, 0.2, 98, 0.0),
-    createData('Marshmallow', 318, 0, 81, 2.0),
-    createData('Nougat', 360, 19.0, 9, 37.0),
-    createData('Oreo', 437, 18.0, 63, 4.0),
-];
 
 function desc(a, b, orderBy) {
     if (b[orderBy] < a[orderBy]) {
@@ -66,11 +50,11 @@ function getSorting(order, orderBy) {
 }
 
 const headCells = [
-    { id: 'name', numeric: false, disablePadding: true, label: 'Address From' },
-    { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-    { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-    { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
-    { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
+    { id: 'addressFrom', numeric: false, disablePadding: true, label: 'Address From', sortable: true},
+    { id: 'addressTo', numeric: false, disablePadding: false, label: 'Address To' },
+    { id: 'appointmentTime', numeric: true, disablePadding: false, label: 'Appointment Time' },
+    { id: 'client', numeric: true, disablePadding: false, label: 'Client', sortable: true },
+    { id: 'status', numeric: true, disablePadding: false, label: 'Status' },
 ];
 
 function EnhancedTableHead(props) {
@@ -91,24 +75,31 @@ function EnhancedTableHead(props) {
                     />
                 </TableCell>
                 {headCells.map(headCell => (
-                    <TableCell
+                    <TableCell  className={classes.tableHeader}
                         key={headCell.id}
                         align={headCell.numeric ? 'right' : 'left'}
                         padding={headCell.disablePadding ? 'none' : 'default'}
                         sortDirection={orderBy === headCell.id ? order : false}
                     >
-                        <TableSortLabel
-                            active={orderBy === headCell.id}
-                            direction={order}
-                            onClick={createSortHandler(headCell.id)}
-                        >
-                            {headCell.label}
-                            {orderBy === headCell.id ? (
-                                <span className={classes.visuallyHidden}>
+                        {!!headCell.sortable ? (
+                            <TableSortLabel
+                                active={orderBy === headCell.id}
+                                direction={order}
+                                onClick={createSortHandler(headCell.id)}
+                            >
+                                {headCell.label}
+                                {orderBy === headCell.id ? (
+                                    <span className={classes.visuallyHidden}>
                   {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
                 </span>
-                            ) : null}
-                        </TableSortLabel>
+                                ) : null}
+                            </TableSortLabel>
+                        ) : (
+                            <span>
+                                {headCell.label}
+                            </span>
+                        )}
+
                     </TableCell>
                 ))}
             </TableRow>
@@ -144,6 +135,10 @@ const useToolbarStyles = makeStyles(theme => ({
     title: {
         flex: '1 1 100%',
     },
+    tableHeader: {
+        'font-weight': 'bold',
+        'border-bottom': 'none'
+    }
 }));
 
 const EnhancedTableToolbar = props => {
@@ -209,16 +204,19 @@ const useStyles = makeStyles(theme => ({
         top: 20,
         width: 1,
     },
+    tableHeader: {
+        'font-weight': 'bold'
+    }
 }));
 
 export default function DriverOrdersTableCustomized() {
     const classes = useStyles();
     const [order, setOrder] = React.useState('asc');
-    const [orderBy, setOrderBy] = React.useState('calories');
+    const [orderBy, setOrderBy] = React.useState('addressFrom');
     const [selected, setSelected] = React.useState([]);
     const [page, setPage] = React.useState(0);
-    const [dense, setDense] = React.useState(false);
     const [rowsPerPage, setRowsPerPage] = React.useState(5);
+    const [dataRows, setDataRows] = React.useState([]);
 
     const handleRequestSort = (event, property) => {
         const isDesc = orderBy === property && order === 'desc';
@@ -226,9 +224,24 @@ export default function DriverOrdersTableCustomized() {
         setOrderBy(property);
     };
 
+    const onOpenedOrdersLoaded = (response) => {
+        setDataRows(
+        response.map((row) => {
+
+            return createData(row.order.addressFrom, row.order.addressTo, row.order.appointmentDate,
+                getUserFullName(row.order.client), getStatusCaption(row.status.titleKey));
+        })
+        );
+    }
+
+    useEffect(() => {
+        makeGetCall("/driver/openedOrders", onOpenedOrdersLoaded);
+
+    }, []);
+
     const handleSelectAllClick = event => {
         if (event.target.checked) {
-            const newSelecteds = rows.map(n => n.name);
+            const newSelecteds = dataRows.map(n => n.addressFrom);
             setSelected(newSelecteds);
             return;
         }
@@ -264,13 +277,12 @@ export default function DriverOrdersTableCustomized() {
         setPage(0);
     };
 
-    const handleChangeDense = event => {
-        setDense(event.target.checked);
-    };
-
     const isSelected = name => selected.indexOf(name) !== -1;
 
-    const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+    const getEmptyRows = (rows) => {
+        return rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+    }
 
     return (
         <div className={classes.root}>
@@ -280,7 +292,7 @@ export default function DriverOrdersTableCustomized() {
                     <Table
                         className={classes.table}
                         aria-labelledby="tableTitle"
-                        size={dense ? 'small' : 'medium'}
+                        size={'medium'}
                         aria-label="enhanced table"
                     >
                         <EnhancedTableHead
@@ -290,23 +302,24 @@ export default function DriverOrdersTableCustomized() {
                             orderBy={orderBy}
                             onSelectAllClick={handleSelectAllClick}
                             onRequestSort={handleRequestSort}
-                            rowCount={rows.length}
+                            rowCount={dataRows.length}
                         />
                         <TableBody>
-                            {stableSort(rows, getSorting(order, orderBy))
+                            {stableSort(dataRows, getSorting(order, orderBy))
                                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                                 .map((row, index) => {
-                                    const isItemSelected = isSelected(row.name);
+                                    const isItemSelected = isSelected(row.addressFrom);
                                     const labelId = `enhanced-table-checkbox-${index}`;
+                                    console.log(row);
 
                                     return (
                                         <TableRow
                                             hover
-                                            onClick={event => handleClick(event, row.name)}
+                                            onClick={event => handleClick(event, row.addressFrom)}
                                             role="checkbox"
                                             aria-checked={isItemSelected}
                                             tabIndex={-1}
-                                            key={row.name}
+                                            key={row.addressFrom}
                                             selected={isItemSelected}
                                         >
                                             <TableCell padding="checkbox">
@@ -316,17 +329,17 @@ export default function DriverOrdersTableCustomized() {
                                                 />
                                             </TableCell>
                                             <TableCell component="th" id={labelId} scope="row" padding="none">
-                                                {row.name}
+                                                {row.addressFrom}
                                             </TableCell>
-                                            <TableCell align="right">{row.calories}</TableCell>
-                                            <TableCell align="right">{row.fat}</TableCell>
-                                            <TableCell align="right">{row.carbs}</TableCell>
-                                            <TableCell align="right">{row.protein}</TableCell>
+                                            <TableCell align="left">{row.addressTo}</TableCell>
+                                            <TableCell align="right">{row.appointmentTime}</TableCell>
+                                            <TableCell align="right">{row.client}</TableCell>
+                                            <TableCell align="right">{row.status}</TableCell>
                                         </TableRow>
                                     );
                                 })}
-                            {emptyRows > 0 && (
-                                <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
+                            {getEmptyRows(dataRows) > 0 && (
+                                <TableRow style={{ height: (33) * getEmptyRows(dataRows) }}>
                                     <TableCell colSpan={6} />
                                 </TableRow>
                             )}
@@ -336,17 +349,13 @@ export default function DriverOrdersTableCustomized() {
                 <TablePagination
                     rowsPerPageOptions={[5, 10, 25]}
                     component="div"
-                    count={rows.length}
+                    count={dataRows.length}
                     rowsPerPage={rowsPerPage}
                     page={page}
                     onChangePage={handleChangePage}
                     onChangeRowsPerPage={handleChangeRowsPerPage}
                 />
             </Paper>
-            <FormControlLabel
-                control={<Switch checked={dense} onChange={handleChangeDense} />}
-                label="Dense padding"
-            />
         </div>
     );
 }
