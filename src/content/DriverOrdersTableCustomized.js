6722ab546a886de809a3fe5a121f6ed6b39c25a0
Dmitry Dyukov <dmitry_dyukov@epam.com>
2020-01-28 23:14:23 +0300

    Move EnhancedTableHead into separate component file.

--- src/content/DriverOrdersTableCustomized.js
+++ src/content/DriverOrdersTableCustomized.js
@@ -6,10 +6,8 @@ import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
@@ -26,6 +24,7 @@ import CompleteButton from "../components/buttons/CompleteButton";
 import RefreshButton from "../components/buttons/RefreshButton";
 import OrderDetails from "./OrderDetails";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import EnhancedTableHead from "../components/table/EnhancedTableHead";
 
 function createData(id, addressFrom, addressTo, appointmentTime, client, status) {
     return { id, addressFrom, addressTo, appointmentTime, client, status };
@@ -66,82 +65,6 @@ function stableSort(array, cmp) {
 function getSorting(order, orderBy) {
     return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
 }
-
-function EnhancedTableHead(props) {
-    const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort, forStatus } = props;
-    const createSortHandler = property => event => {
-        onRequestSort(event, property);
-    };
-    const headCells = forStatus === 'closed' ? [
-        { id: 'addressFrom', numeric: false, label: 'Address From'},
-        { id: 'addressTo', numeric: false, disablePadding: false, label: 'Address To' },
-        { id: 'appointmentTime', numeric: true, disablePadding: false, label: 'Appointment Time' },
-        { id: 'client', numeric: true, disablePadding: false, label: 'Client', align: 'center' }
-    ]: [
-        { id: 'addressFrom', numeric: false, disablePadding: true, label: 'Address From', sortable: true},
-        { id: 'addressTo', numeric: false, disablePadding: false, label: 'Address To' },
-        { id: 'appointmentTime', numeric: true, disablePadding: false, label: 'Appointment Time', sortable: true },
-        { id: 'client', numeric: true, disablePadding: false, label: 'Client', align: 'center' },
-        { id: 'actions', numeric: false, disablePadding: true, label: 'Actions', align: 'center'}
-    ];
-
-    const getCheckboxCell = (numSelected, rowCount, onSelectAllClick) => {
-        return forStatus !== 'closed' ? (<TableCell padding="checkbox">
-            <Checkbox
-                indeterminate={numSelected > 0 && numSelected < rowCount}
-                checked={numSelected === rowCount && rowCount > 0}
-                onChange={onSelectAllClick}
-                inputProps={{ 'aria-label': 'select all desserts' }}
-            />
-        </TableCell>) : null;
-    };
-
-    return (
-        <TableHead>
-            <TableRow>
-                {getCheckboxCell(numSelected, rowCount, onSelectAllClick)}
-                {headCells.map(headCell => (
-                    <TableCell  className={classes.tableHeader}
-                        key={headCell.id}
-                        align={headCell.align ? headCell.align : (headCell.numeric ? 'right' : 'left')}
-                        padding={headCell.disablePadding ? 'none' : 'default'}
-                        sortDirection={orderBy === headCell.id ? order : false}
-                    >
-                        {!!headCell.sortable ? (
-                            <TableSortLabel
-                                active={orderBy === headCell.id}
-                                direction={order}
-                                onClick={createSortHandler(headCell.id)}
-                            >
-                                {headCell.label}
-                                {orderBy === headCell.id ? (
-                                    <span className={classes.visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </span>
-                                ) : null}
-                            </TableSortLabel>
-                        ) : (
-                            <span>
-                                {headCell.label}
-                            </span>
-                        )}
-
-                    </TableCell>
-                ))}
-            </TableRow>
-        </TableHead>
-    );
-}
-
-EnhancedTableHead.propTypes = {
-    classes: PropTypes.object.isRequired,
-    numSelected: PropTypes.number.isRequired,
-    onRequestSort: PropTypes.func.isRequired,
-    onSelectAllClick: PropTypes.func.isRequired,
-    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-    orderBy: PropTypes.string.isRequired,
-    rowCount: PropTypes.number.isRequired,
-};
 
 const useToolbarStyles = makeStyles(theme => ({
     root: {
@@ -292,6 +215,19 @@ export default function DriverOrdersTableCustomized(props) {
     const getOrdersUrl = statuses === 'opened' ?
         '/driver/openedOrders' : statuses === 'assigned' ?
             '/driver/assignedOrders' : statuses ==='closed' ? '/driver/completedOrders' : 'null';
+
+    const headCells = statuses === 'closed' ? [
+        { id: 'addressFrom', numeric: false, label: 'Address From'},
+        { id: 'addressTo', numeric: false, disablePadding: false, label: 'Address To' },
+        { id: 'appointmentTime', numeric: true, disablePadding: false, label: 'Appointment Time' },
+        { id: 'client', numeric: true, disablePadding: false, label: 'Client', align: 'center' }
+    ]: [
+        { id: 'addressFrom', numeric: false, disablePadding: true, label: 'Address From', sortable: true},
+        { id: 'addressTo', numeric: false, disablePadding: false, label: 'Address To' },
+        { id: 'appointmentTime', numeric: true, disablePadding: false, label: 'Appointment Time', sortable: true },
+        { id: 'client', numeric: true, disablePadding: false, label: 'Client', align: 'center' },
+        { id: 'actions', numeric: false, disablePadding: true, label: 'Actions', align: 'center'}
+    ];
 
     const handleRequestSort = (event, property) => {
         const isDesc = orderBy === property && order === 'desc';
@@ -479,6 +415,7 @@ export default function DriverOrdersTableCustomized(props) {
                             aria-label="enhanced table"
                         >
                             <EnhancedTableHead
+                                headCells={headCells}
                                 classes={classes}
                                 numSelected={selected.length}
                                 order={order}
