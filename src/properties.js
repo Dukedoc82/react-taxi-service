e81ee91e4cc887e3bf07b01c57da1d03a8599db4
Dmitrii Dyukov <dmitrii.dyukov@kronos.com>
2020-03-02 16:46:11 +0300

    Change backendServer prop

--- src/properties.js
+++ src/properties.js
@@ -1,6 +1,6 @@
 export const properties = {
     //backendServer: 'http://epruryaw0808:8080'
-    backendServer: 'http://taxi-epam:8080'
+    backendServer: 'http://taxi-epam.ru:8080'
 };
 
 export default properties
